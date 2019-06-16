9200e1fe39c1665800eba86fc8fced5490c93923
Timur Bilotserkivky <timur.belotserkovsky@gmail.com>
2019-06-16 18:12:46 +0300

    task 1 done, added two dices

--- app.js
+++ app.js
@@ -10,34 +10,47 @@ GAME RULES:
 */
 
 const RESET_VALUE = 1;
+const VIN_VALUE = 100;
 
 let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
-const diceElement = document.querySelector('.dice');
+const diceElement_1 = document.getElementById('firstDice');
+const diceElement_2 = document.getElementById('secondDice');
 
 const initGame = () => {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
-  diceElement.style.display = 'none';
+  diceElement_1.style.display = 'none';
+  diceElement_2.style.display = 'none';
+  document.querySelector('.btn-roll').style.display = 'block';
+  document.querySelector('.btn-hold').style.display = 'block';
+  scores = [0, 0];
+  current = 0;
 }
 
 initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  let dice = Math.floor(Math.random() * 6) + 1;
+  let dice_1 = Math.floor(Math.random() * 6) + 1;
+  let dice_2 = Math.floor(Math.random() * 6) + 1;
 
-  diceElement.src = `dice-${dice}.png`;
-  diceElement.style.display = 'block';
+  diceElement_1.src = `dice-${dice_1}.png`;
+  diceElement_1.style.display = 'block';
+  diceElement_2.src = `dice-${dice_2}.png`;
+  diceElement_2.style.display = 'block';
 
-  if (dice !== RESET_VALUE) {
-    current += dice;
+  if (dice_1 !== RESET_VALUE && dice_2 !== RESET_VALUE) {
+    current += dice_1 + dice_2;
     document.getElementById('current-'+activePlayer).textContent = current;
 
-    if (scores[activePlayer] + current >= 20) {
-      alert(`Player ${activePlayer} won!!!`);
+    if (scores[activePlayer] + current >= VIN_VALUE) {
+      alert(`Player ${activePlayer+1} won!!!`);
+      document.querySelector('.btn-roll').style.display = 'none';
+      document.querySelector('.btn-hold').style.display = 'none';
+
     }
     
   } else {
@@ -50,7 +63,8 @@ const changePlayer = () => {
   document.getElementById('current-'+activePlayer).textContent = 0;
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
   activePlayer = +!activePlayer;
-  diceElement.style.display = 'none';
+  diceElement_1.style.display = 'none';
+  diceElement_2.style.display = 'none';
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
 }
 
